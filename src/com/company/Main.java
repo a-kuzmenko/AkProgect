7ac83313f585658f32329f7497b7a71fa3f588f3
akuzmenko <kyznechik140@gmail.com>
2019-01-27 11:20:26 +0200

    Merge branch 'master' of https://github.com/a-kuzmenko/AkProgect
    # Conflicts:
    #	src/com/company/Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("world");
-        System.out.println("world");
+        System.out.println("wo123rld");
         System.out.println("wo123rld");
 
 
