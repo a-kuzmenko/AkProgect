c13a5dc3178050d2d6f45709d9138b03d5f66233
akuzmenko <kyznechik140@gmail.com>
2019-01-27 09:45:23 +0200

    commit4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,9 @@ public class Main {
         System.out.println("hi");
         System.out.println("hi");
         System.out.println("world");
-        System.out.println("world");
-        System.out.println("world");
-        System.out.println("world xdfgb44444fb");
+        System.out.println("wocgnrld");
+        System.out.println("worldcgnhgc");
+        System.out.println("world xdfhngnhfgb44444fb");
         System.out.println("hihbcf4444444bc");
         System.out.println("hihb4444444cfbc");
 
